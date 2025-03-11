288b623e72cbfccf08bf501afad5a874fe8d6cc6
edperlman <edyarasheff@gmail.com>
2025-03-11 11:21:05 +1100

    WIP: Prepare for expo upgrade

---
+++ packages/mobile_app/index.js
@@ -0,0 +1,8 @@
+// 1) If you use expo-dev-client or dev builds, you can import it here
+// import 'expo-dev-client';
+
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// 2) Register your main App component
+registerRootComponent(App);
